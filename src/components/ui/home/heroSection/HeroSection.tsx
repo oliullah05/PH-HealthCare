81ebca41ee9b8282651596ee9b45c8637b806aa0
oliullah05 <mdolihasan@gmail.com>
2024-05-01 12:23:44 +0600

    Creating The Hero Section Segment-2

--- src/components/ui/home/heroSection/HeroSection.tsx
+++ src/components/ui/home/heroSection/HeroSection.tsx
@@ -5,22 +5,22 @@ import assets from "@/assets";
 const HeroSection = () => {
     return (
         <Container sx={{
-            display:"flex",
-            direction:"row",
-            my:"16"
+            display: "flex",
+            direction: "row",
+            my: "16"
         }}>
             <Box sx={{
-                flex:1,
-                position:"relative",
-                padding:"5px"
+                flex: 1,
+                position: "relative",
+                padding: "5px"
 
             }}>
                 <Box sx={
                     {
-                        position:"absolute",
-                        width:"700px",
-                        top:"-90px",
-                        left:"-120px"
+                        position: "absolute",
+                        width: "700px",
+                        top: "-90px",
+                        left: "-120px"
                     }
                 }>
                     <Image src={assets.svgs.grid} alt="Grid"></Image>
@@ -30,19 +30,64 @@ const HeroSection = () => {
                 <Typography variant="h3" component="h1" fontWeight={600}>Come From</Typography>
                 <Typography color="primary.main" variant="h3" component="h1" fontWeight={600}>Preventive Care</Typography>
 
-                <Typography  variant="h6" component="p" fontWeight={400} sx={{
-                    width:"50%"
-                }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos provident tenetur expedita repudiandae possimus quo dolore incidunt, cum eius inventore.</Typography>
-                <Button>Make Appointment</Button>
-                <Button variant="outlined" sx={
-                    {
-                        marginLeft:"5px"
-                    }
-                }>Contact Us</Button>
+                <Typography variant="h6" component="p" fontWeight={200} >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos provident tenetur expedita repudiandae possimus quo dolore incidunt, cum eius inventore.</Typography>
+                <Box sx={{
+                    display: "flex",
+                    gap: "7px",
+                    marginTop: "10px",
+                    marginBottom: "10px"
+                }}>
+                    <Button>Make Appointment</Button>
+                    <Button variant="outlined" >Contact Us</Button>
+                </Box>
             </Box>
-            
-            
-            <Box>Right</Box>
+
+
+
+            {/*  */}
+            <Box sx={{
+                padding: 1,
+                flex: 1,
+                display: "flex",
+                justifyContent: "center",
+                position: "relative",
+                marginTop: 0
+            }}>
+
+                <Box sx={{ position: "absolute", left: "200px", top: "-30px" }}>
+                    <Image src={assets.svgs.arrow} width={100} height={100} alt="Arrow">
+                    </Image>
+                </Box>
+
+                <Box sx={{
+                    display: "flex",
+                    gap: 2
+                }}>
+                    <Box mt={4}><Image src={assets.images.doctor1} alt="doctor1" width={240} height={380}></Image></Box>
+                    <Box><Image src={assets.images.doctor2} alt="doctor2" width={240} height={350}></Image></Box>
+                </Box>
+
+                {/*  */}
+                <Box sx={{
+                    position: "absolute",
+                    top: "220px",
+                    left: "150px"
+                }}>
+                    <Image src={assets.images.doctor3} alt="doctor3" width={240} height={240}>
+                    </Image>
+                </Box>
+
+
+                <Box sx={{
+                    position: "absolute",
+                bottom:"-50px",
+                right:0,
+                zIndex:"-1"
+                }}>
+                    <Image src={assets.images.stethoscope} alt="stethoscope" width={180} height={180}>
+                    </Image>
+                </Box>
+            </Box>
         </Container>
     );
 };
